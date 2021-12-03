956bee9388b266225d00ff783f9d98cc78087e1c
André Alfonso Peixoto <alfonso.peixoto@gmail.com>
2021-12-03 18:47:39 -0500

    Update DetermineTheta.m

--- DetermineTheta.m
+++ DetermineTheta.m
@@ -1,106 +1,93 @@
-function theta_value=DetermineTheta(x_value,phi_value)
+% Função para determiner o ângulo theta
+function theta_value=DetermineTheta(x_value,y_value,phi_value)
 
-%
-% Funçao que determine o angulo theta do problema backing-up a truck.
-%
+PosicaoX=[-10,0,10, 35
+    	   30,45,45,65
+	       60,70,70,80
+	       75,80,80,85
+	       80,90,100,110];
 
-Posicao=[-10,0,10,35
- 	30,40,40,50
-	45,50,50,55
-	50,60,60,70
-	65,90,100,110];
-%  
-%  % Gerar gráfico da posição;
-%  t=linspace(0,100,101);
-%  for i=1:5
-%    Pvalues(:,i)=TRAP(t,Posicao(i,:))';
-%  end
-%  plot(t,Pvalues,'linewidth',4);
-%  axis([0 100 0 1.1]);
-%  text(0,1.05,'X1');
-%  text(40,1.05,'X2');
-%  text(50,1.05,'X3');
-%  text(60,1.05,'X4');
-%  text(90,1.05,'X5');
-%  grid;
-%  print -depsc mfposicao.eps
-    
-Phi=[ -95,-45,9
-  -9,23,54
-  36,63,90
-  72,90,108
-  90,117,144
-  126,157,189
-  171,225,275];
+PosicaoY=[-10, 0,20,45
+ 	       40,60,60,75
+	       70,80,80,90
+	       85,95,95,105
+	       100,105,110,115];
 
-%  % Gerar gráfico do angulo phi;
-%  t=linspace(-90,270,361);
-%  for i=1:7
-%    Phivalues(:,i)=TRI(t,Phi(i,:))';
-%  end
-%  plot(t,Phivalues,'linewidth',4);
-%  axis([-90 270 0 1.1]);
-%  text(-45,1.05,'Phi1');
-%  text(23,1.05,'Phi2');
-%  text(63,1.05,'Phi3');
-%  text(90,1.05,'Phi4');
-%  text(117,1.05,'Phi5');
-%  text(157,1.05,'Phi6');
-%  text(225,1.05,'Phi7');
-%  grid;
-%  print -depsc mfphi.eps    
+Phi=[ -95,-45, 9
+       -9, 23,54
+       36, 63,90
+       72,90,108
+       90,117,144
+      126,157,189
+      171,225,275];
 
 Theta=[-45,-30,-15
-  -25,-15,-5
-  -12,-6,0
-  -5,0,5
-  0,6,12
-  5,15,25
-  15,30,45];
+       -25,-15, -5
+       -12, -6,  0
+       - 5,  0,  5
+         0,  6, 12
+         5, 15, 25
+        15, 30, 45];
 
-%  % Gerar gráfico do angulo theta;
-%  t=linspace(-30,30,61);
-%  for i=1:7
-%    Thetavalues(:,i)=TRI(t,Theta(i,:))';
-%  end
-%  plot(t,Thetavalues,'linewidth',4);
-%  axis([-30 30 0 1.1]);
-%  text(-30,1.05,'T1');
-%  text(-15,1.05,'T2');
-%  text(-6,1.05,'T3');
-%  text(0,1.05,'T4');
-%  text(6,1.05,'T5');
-%  text(15,1.05,'T6');
-%  text(25,1.05,'T7');
-%  grid;
-%  print -depsc mftheta.eps    
-
-Rules=[5 5 6 6 6
-  5 5 6 6 7
-  3 3 4 4 4
-  3 3 3 5 6
-  2 2 3 3 3
-  2 2 4 6 6
-  2 2 3 3 3]';
-
-%{
-Rules=[5 6 6 7 7
-  3 5 6 7 7
-  2 3 5 6 7
-  2 2 4 6 6
-  1 2 3 5 6
-  1 1 2 3 5
-  1 1 2 2 3]';
-%}
-
+Rules=[6 5 6 5 6
+       5 2 5 5 6
+       2 1 5 5 5
+       1 1 1 4 5     %B1
+       1 1 1 3 3 
+       1 1 7 1 3
+       2 1 7 7 2
+       
+       6 5 6 5 7
+       5 5 5 5 7
+       2 2 5 5 7
+       1 1 1 4 6     %B2
+       1 1 1 3 3
+       1 1 7 1 3
+       2 1 7 7 2
+       
+       6 5 6 5 7
+       5 5 5 5 7
+       2 2 5 5 7
+       1 1 1 4 7     %B3
+       1 1 1 3 6
+       1 1 7 2 6
+       2 1 7 1 2
+       
+       5 6 4 7 1
+       4 4 4 6 1
+       2 1 4 5 1
+       1 1 1 4 5     %B4
+       7 7 1 3 3
+       7 7 7 2 2
+       6 7 6 1 1
+       
+       4 4 6 7 1
+       4 4 5 6 1
+       2 2 5 5 1
+       1 1 1 4 6      %B5
+       1 1 1 3 5
+       7 7 7 2 2
+       6 7 6 1 1]';
+   
 t=linspace(-30,30,61);  
 mftheta=zeros(size(t));
-for i=1:size(Posicao,1);
-  for j=1:size(Phi,1);
-    w=min(TRAP(x_value,Posicao(i,:)),TRI(phi_value,Phi(j,:)));
-    mftheta=max(mftheta,min(w,TRI(t,Theta(Rules(i,j),:))));
+for i=1:size(PosicaoX,1);
+  for j=1:size(PosicaoY,1);
+      for k=1:size(Phi,1);
+         w=min(TRAP(x_value,PosicaoX(i,:)),TRI(phi_value,Phi(k,:)));
+         w=min(w,TRAP(y_value,PosicaoY(j,:)));            
+         mftheta=max(mftheta,min(w,TRI(t,Theta(Rules(i,k),:))));
+      end
   end
 end
 
 % Defuzzificacao;
 theta_value=sum(t.*mftheta)/sum(mftheta);
+
+%[95,5,45]
+%[97,5,45]
+%[97,5,270]
+%[10,10,45]
+%[85,15,-90]
+%[10,50,-90]
+%[10,10,-90]
